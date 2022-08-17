a69bb935a0dc793c0e66c88d290d8a32355a9aa6
gyur1kim <qri1114@gmail.com>
2022-08-17 13:26:45 +0900

    0817_stack

---
+++ 0817_stack/stack.py
@@ -0,0 +1,52 @@
+class Stack:
+
+    def __init__(self, size):
+        self.size = size
+        self.data = [None]*self.size
+        self.top = -1
+
+    def __str__(self):
+        return f'{self.data}'
+
+    def push(self, value):
+        if self.isFull():
+            print('overflow')
+        else:
+            self.top += 1
+            self.data[self.top] = value
+
+    def pop(self):
+        if self.isEmpty():
+            print('underflow')
+            return 0
+        else:
+            self.top -= 1
+            return self.data[self.top+1]
+
+    def isEmpty(self):
+        # if self.top == -1:
+        #     return True
+        # else:
+        #     return False
+        return self.top == -1
+
+    def isFull(self):
+        return self.top+1 == self.size
+
+
+size = 10
+stack = Stack(3)
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+print(stack)
+print(stack.isEmpty())
+print(stack.isFull())
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+print(stack)
+print(stack.isEmpty())
+print(stack.isFull())
