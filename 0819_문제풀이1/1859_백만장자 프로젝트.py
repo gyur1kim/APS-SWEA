4be65a771d95e621af9b9a54c4f343c90cf53a6c
gyur1kim <qri1114@gmail.com>
2022-08-19 01:11:57 +0900

    0819_도전중

---
+++ 0819_문제풀이1/1859_백만장자 프로젝트.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('1859.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    days = int(input())
+    prices = list(map(int, input().split()))
+
+
+
+
+
+    print(f'#{tc}')
